98f9d112d5924aa87391a803de4ef8544bb4acd7
Tomasz Komoszesk <tomasz.komoszeski@gmail.com>
2018-08-19 20:56:00 +0200

    fix absolute navigation

--- Blog.Web/ClientApp/src/app/navigation/navigation.component.ts
+++ Blog.Web/ClientApp/src/app/navigation/navigation.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  public isCollapsed =  false;
+
+  constructor() {
+    this.isCollapsed = false;
+  }
 
   ngOnInit() {
   }
