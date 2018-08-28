b475fa171621830b1b044205ff8e66d48e09c3a1
Tomasz <tomasz.komoszeski@gmail.com>
2018-08-28 15:59:48 +0200

    Start working on own http service for catching errors, loading data etc

---
+++ Blog.Web/ClientApp/src/app/core/http.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpService  {
+
+    constructor(private http: HttpClient){
+
+    }
+
+    public get<T>(url: string): Observable<T>{
+        return this.http.get<T>(url)
+    }
+
+    public post<T>(url: string,body: any): Observable<T>{
+        return this.http.post<T>(url,body)
+    }
+
+    public put<T>(url: string, body: any): Observable<T> {
+        return this.http.put<T>(url,body);
+    }
+
+    public delete<T>(url: string): Observable<T>{
+        return this.http.delete<T>(url);
+    }
+}
