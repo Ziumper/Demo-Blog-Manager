ea80c43a141829604104976d2f39836b0840a5eb
Tomasz <tomasz.komoszeski@gmail.com>
2018-08-30 14:09:56 +0200

    Adding blog manager in table

--- Blog.Web/ClientApp/src/app/app-routing.module.ts
+++ Blog.Web/ClientApp/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AddBlogComponent } from './blog/add-blog/add-blog.component';
 import { EditBlogComponent } from './blog/eidt-blog/edit-blog.component';
+import { BlogsListComponent } from './blog/blogs-list/blogs-list.component';
 
 const routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'blogs', component: BlogsListComponent},
   { path: 'add-blog', component: AddBlogComponent },
   { path: 'edit-blog/:id', component: EditBlogComponent },
   { path: '**', redirectTo: 'home' }
