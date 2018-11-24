ad0a6a47f0c0b9963a2271f9c09d25a582e28456
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2018-11-24 14:17:46 +0100

    implemented blog component and search component

--- Blog.Web/ClientApp/src/app/blog/blog.component.ts
+++ Blog.Web/ClientApp/src/app/blog/blog.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostListConfig } from '../core/config/post-list.config';
+import { BlogService } from './blog.service';
+import { BlogModel } from './models/blog.model';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent extends PostListConfig {
+export class BlogComponent extends PostListConfig implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  public blog: BlogModel;
+
+  constructor(private route: ActivatedRoute, private blogService: BlogService) {
     super();
+  }
+
+  public ngOnInit(): void {
+    const blogId = this.route.snapshot.params['blogId'];
+    if (blogId) {
+      this.blogService.getBlogById(blogId).subscribe(response => {
+        this.blog = response;
+      });
+      this.getPosts();
+    }
   }
 
   public getPosts(): void {
