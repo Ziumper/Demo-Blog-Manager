78c98e29d999d143d4254dff01f1f434a170ce0f
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2018-12-07 20:08:30 +0100

    return of postslist component

--- Blog.Web/ClientApp/src/app/post/posts-list/posts-list.component.ts
+++ Blog.Web/ClientApp/src/app/post/posts-list/posts-list.component.ts
@@ -12,26 +12,43 @@ import { ActivatedRoute } from '@angular/router';
 
 export class PostsListComponent implements OnInit {
 
-    @Input()
+
     public posts: Array<PostModel>;
+    public collectionSize: number;
+    public page: number;
+    public pageSize: number;
+    public postQueryModel: PostQueryModel;
 
-    @Input()
-    public collectionSize: Number;
-
-    @Input()
-    public page: Number;
-
-    @Input()
-    public pageSize: Number;
-
-    constructor() {
+    constructor(private postService: PostService,
+        private activatedRoute: ActivatedRoute) {
         this.posts = new Array<PostModel>();
         this.collectionSize = 0;
-        this.page = 0;
-        this.pageSize = 0;
+        this.page = 1;
+        this.pageSize = 5;
+        this.postQueryModel = new PostQueryModel(this.page, 5, 1, true, '', [0], 0);
     }
 
     public ngOnInit(): void {
+
+    }
+
+    public onPageChange(page: number): void {
+        this.postQueryModel.page = page;
+        this.getPosts();
+    }
+
+    public getPosts(): void {
+        const blogId = this.activatedRoute.snapshot.params['blogId'];
+        if (blogId) {
+            this.postQueryModel.blogId = blogId;
+            this.postService.getPostsPagedByBlogId(this.postQueryModel)
+            .subscribe(response => {
+                this.posts = response.entities;
+                this.pageSize = response.size;
+                this.collectionSize = response.count;
+                this.page = response.page;
+            });
+        }
 
     }
 
