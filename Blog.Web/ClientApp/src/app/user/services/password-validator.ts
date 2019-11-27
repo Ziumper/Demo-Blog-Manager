5d10428b87b6199c9640aafebe49b4891a94cb85
komoszeskito <Tomasz.Komoszeski@sunzinet.com>
2019-11-27 22:26:26 +0100

    [FEATURE] Add password validator component.

---
+++ Blog.Web/ClientApp/src/app/user/services/password-validator.ts
@@ -0,0 +1,52 @@
+import { Injectable } from "@angular/core";
+import { Validators, FormControl, ValidatorFn, AbstractControl } from "@angular/forms";
+
+@Injectable()
+export class PasswordValidatorService {
+
+    private passwordPattern: string;
+
+    constructor(){ 
+        this.passwordPattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>])[a-zA-Z0-9!@#$%^&*(),.?":{}|<>]+$';
+    }
+
+    public createPasswordControl() {
+        const passwordControl = new FormControl('', [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern(this.passwordPattern)
+        ]);
+        return passwordControl;
+    }
+
+    public createRepeatedPasswordControl(passwordControl : FormControl) {
+       const repeatedPasswordControl = new FormControl('', 
+       [
+           Validators.required, 
+           this.repeatedValidatorFn(passwordControl)
+        ]);
+
+        return repeatedPasswordControl;
+    }
+
+    /**
+      * It is custom validator for checking password repeating
+      * @param control FormControl
+    */
+   private repeatedValidatorFn(control: FormControl): ValidatorFn {
+    return (c: AbstractControl): { [key: string]: boolean } | null => {
+        const originalPassword = control.value;
+        const isNotSame = c.value !== originalPassword;
+
+        if (isNotSame) {
+            return { 'repeated': true };
+        }
+
+        return null;
+    };
+}
+
+
+    
+
+}
