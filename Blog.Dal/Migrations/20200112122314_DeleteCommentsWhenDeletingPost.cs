1b80b74a05edc4b1a7372553e064ac0d04f1279e
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2020-01-12 13:24:25 +0100

    [FEATURE] Add migration for deleting comments with post

---
+++ Blog.Dal/Migrations/20200112122314_DeleteCommentsWhenDeletingPost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Dal.Migrations
+{
+    public partial class DeleteCommentsWhenDeletingPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreationDate", "ModificationDate" },
+                values: new object[] { new DateTime(2020, 1, 12, 13, 23, 14, 562, DateTimeKind.Local), new DateTime(2020, 1, 12, 13, 23, 14, 562, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreationDate", "ModificationDate" },
+                values: new object[] { new DateTime(2020, 1, 12, 13, 23, 14, 559, DateTimeKind.Local), new DateTime(2020, 1, 12, 13, 23, 14, 561, DateTimeKind.Local) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreationDate", "ModificationDate" },
+                values: new object[] { new DateTime(2020, 1, 11, 12, 10, 1, 308, DateTimeKind.Local), new DateTime(2020, 1, 11, 12, 10, 1, 308, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreationDate", "ModificationDate" },
+                values: new object[] { new DateTime(2020, 1, 11, 12, 10, 1, 306, DateTimeKind.Local), new DateTime(2020, 1, 11, 12, 10, 1, 308, DateTimeKind.Local) });
+        }
+    }
+}
