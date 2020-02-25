df12942adf5502d69c200d67910f9f2df01b9c51
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2020-02-25 23:51:43 +0100

    [FEATURE] Added modal delete account confirmation

--- Blog.Web/ClientApp/src/app/user/profile/profile.component.ts
+++ Blog.Web/ClientApp/src/app/user/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from '../models/user.model';
 import { AuthenticationService } from '../services/authentication.service';
 import { Subscription } from 'rxjs';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-user-profile',
@@ -12,15 +13,18 @@ import { Subscription } from 'rxjs';
 })
 export class ProfileComponent implements OnInit {
 
+    private closeResult: string;
     private userId;
     private loginSubscription: Subscription;
 
     public user: User;
     public showEditFunctions: boolean;
+    public modal: NgbModalRef;
 
     constructor( private userSerivce: UserService,
         private activatedRoute: ActivatedRoute,
-        private authenticationService: AuthenticationService) {
+        private authenticationService: AuthenticationService,
+        private modalService: NgbModal) {
             this.user = new User();
             this.userId = this.getUserIdFromParams();
             this.showEditFunctions = false;
@@ -61,5 +65,28 @@ export class ProfileComponent implements OnInit {
         }
 
         return false;
+     }
+
+ public open(content) {
+    this.modal = this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  }
+
+  public closeModal() {
+      this.modal.close();
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+    public deleteAccount() {
+        const userId = Number(this.activatedRoute.snapshot.params['userId']);
+        this.userSerivce.delete(userId);
      }
 }
