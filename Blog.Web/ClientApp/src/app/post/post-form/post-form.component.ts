34e5add03b4354989bbaa8fe4e1feee1afab6498
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2020-04-04 15:48:44 +0200

    [FEATURE] Enhanced adding new posts.
    - Add alert after adding the post
    - Add redirect after adding the post

--- Blog.Web/ClientApp/src/app/post/post-form/post-form.component.ts
+++ Blog.Web/ClientApp/src/app/post/post-form/post-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { PostModel } from '../models/post.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PostService } from '../post.service';
+import { AlertService } from 'src/app/core/services/alert.service';
 
 
 @Component({
@@ -14,7 +15,9 @@ export class PostFormComponent implements OnInit {
     public tagName: String;
     public url: String;
 
-    constructor(private postService: PostService, private route: ActivatedRoute) {
+    constructor(private postService: PostService, private route: ActivatedRoute,
+        private alertService: AlertService,
+        private router: Router) {
         this.model = new PostModel();
     }
 
@@ -34,7 +37,10 @@ export class PostFormComponent implements OnInit {
             this.model.id = id;
             this.postService.updatePost(model).subscribe();
         } else {
-            this.postService.addPost(model).subscribe();
+            this.postService.addPost(model).subscribe(response => {
+                this.alertService.success('Post succesfully added', true);
+                this.router.navigate(['/', 'blog-manager', this.model.blogId, 'blog']);
+            });
         }
     }
 
