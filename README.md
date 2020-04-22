b24a2b3031511976c8f06a2db43311a05b1104ff
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2020-04-22 22:05:40 +0200

    [FEATURE] Fully setuped docker c# envirionemnt
    - with stop , recompile, and setup methods.
    - faster recompile
    - add volumes to specific folders and filles.

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 Blog management application in .net core and angular 2. Application is manged in docker. Currently only dev version.
 
-## Build and run
+## Setup
 
 To build and run dev version use this script:
 
 ```bash
-bash scripts/dev.sh
+bash scripts/setup.sh
 ```
 
 Application should be avialibale on this addres
@@ -25,7 +25,7 @@ docker-compose stop
 To update the C# code inside docker container first stop the containers and then rebuild to copy new code.
 
 ```bash
-In progress
+bash scripts/recompile.sh
 ```
 
 ## Debug
--- scripts/recompile.sh
+++ scripts/recompile.sh
@@ -1,19 +1,7 @@
 echo "Killing dotnet process"
 docker exec -ti blog_web_1 sh -c "bash scripts/kill.sh"
-#Copy 
-echo "copy files"
-# docker cp ./Blog.Bll/Dto blog_web_1:/app/Blog.Bll/Dto
-# docker cp ./Blog.Bll/Exceptions blog_web_1:/app/Blog.Bll/Exceptions
-# docker cp ./Blog.Bll/Middlewares blog_web_1:/app/Blog.Bll/Middlewares
-# docker cp ./Blog.Bll/Services blog_web_1:/app//Blog.Bll/Services
-# docker cp ./Blog.Dal/Migrations blog_web_1:/app/Blog.Dal/Migrations
-# docker cp ./Blog.Dal/Models blog_web_1:/app/Blog.Dal/Models
-# docker cp ./Blog.Dal/Repositories blog_web_1:/app//Blog.Dal/Repositories
-# docker cp ./Blog.Web/Controllers blog_web_1:/app/Blog.Web/Controllers
-# docker cp ./Blog.Web/Mappings  blog_web_1:/app/Blog.Web/Mappings
-echo "end copy files"
 #Dotnet database update
-#docker exec -ti blog_web_1 sh -c "cd Blog.Dal && dotnet ef database update" 
+docker exec -ti blog_web_1 sh -c "cd Blog.Dal && dotnet ef database update" 
 #Dotnet restore
 echo "Dotnet restore"
 docker exec -ti blog_web_1 sh -c "cd Blog.Web && dotnet restore"
