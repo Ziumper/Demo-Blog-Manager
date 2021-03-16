cb7e7041795c3e8ae39607446dff32e3fd9961da
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2021-03-16 20:46:14 +0100

    Add windows 10 wsl support

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 run_cmd="dotnet bin/Debug/netcoreapp2.1/Blog.Web.dll --server.urls http://*:80"
 
-until cd /app/Blog.Dal && dotnet ef database update; do
+until dotnet ef database update --startup-project /app/Blog.Web --project /app/Blog.Dal; do
 >&2 echo "SQL Server is starting up"
 sleep 1
 done
